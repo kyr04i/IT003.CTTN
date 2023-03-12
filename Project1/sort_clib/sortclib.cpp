e83df0a9fbae31e28290a0a0b0d9b66cc36074bb
w1n_gl0ry <duynguyen45608@gmail.com>
2023-03-12 11:40:14 +0700

    Add files via upload

---
+++ Project1/sort_clib/sortclib.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <sys/time.h>
+
+int main(){
+    
+    FILE *fd = fopen("./testcase", "r");
+    int *buffer = (int *)calloc(1000000, sizeof(int));
+    for (int i = 0; i<10; ++i)
+    {
+        for (int j = 0; j<1000000; ++j)
+        {
+            fscanf(fd, "%d", &buffer[j]);
+        }
+        printf("Test %d ", i+1);
+        struct timeval stop, start;
+        gettimeofday(&start, NULL);
+        std::sort(buffer, buffer+1000000);
+        gettimeofday(&stop, NULL);
+        double s = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+        printf(" took %fs\n", s);
+    }
+}
