62c160f3afd6ba8eee0abc3cb11fbe464dc4f89d
w1n_gl0ry <duynguyen45608@gmail.com>
2023-06-04 20:25:10 +0700

    Add files via upload

---
+++ Project3/kruskal.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int N = 1e5 + 7;
+int root[N];
+
+int FindRoot(int u) {
+    if (u == root[u]) {
+        return u;
+    }
+    return root[u] = FindRoot(root[u]);
+}
+
+void UnionSet(int u, int v) {
+    int rootu = FindRoot(u);
+    int rootv = FindRoot(v);
+    if (rootu != rootv) {
+        root[rootu] = rootv;
+    }
+}
+
+struct krus {
+    int w, u, v;
+};
+
+bool cmp(krus a, krus b) {
+    return a.w <= b.w;
+}
+
+void pwn() {
+    ifstream input("Graph.TXT");
+    ofstream output("Kruskal.TXT");
+
+    int n, m;
+    input >> n;
+    m = n * n;
+
+    for (int i = 1; i <= m; ++i) {
+        root[i] = i;
+    }
+
+    vector<krus> a(m);
+    int index = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            int w;
+            input >> w;
+            if (j > i) {
+                a[index] = {w, i, j};
+                index++;
+            }
+        }
+    }
+
+    sort(a.begin(), a.end(), cmp);
+
+    int res = 0;
+    for (auto c : a) {
+        if (FindRoot(c.u) != FindRoot(c.v)) {
+            res += c.w;
+            UnionSet(c.u, c.v);
+        }
+    }
+
+    double roundedRes = round(res * 100.0) / 100.0;
+    output << fixed << setprecision(2) << roundedRes;
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    pwn();
+    return 0;
+}
